ea0561ec3691242c1f382824b0a0913b245ec930
qnxu <qiannanxu@creditease.cn>
2017-04-01 11:29:27 +0800

    Add save handler

--- wiki.go
+++ wiki.go
@@ -30,17 +30,31 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
 	page, err := loadPage(title)
 
+	if err != nil {
+		http.Redirect(w, r, "/edit/" + title, http.StatusFound)
+		return
+	}
+
 	renderTemplate(err, w, page, "view.html")
 }
 
 func editHandler(writer http.ResponseWriter, request *http.Request) {
-	title := request.URL.Path[len("/view/"):]
+	title := request.URL.Path[len("/edit/"):]
 	page, err := loadPage(title)
 	if err != nil {
 		page = &Page{Title: title}
 	}
 
 	renderTemplate(err, writer, page, "edit.html")
+}
+
+func saveHandler(writer http.ResponseWriter, request *http.Request) {
+	title := request.URL.Path[len("/save/"):]
+	body := request.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+
+	http.Redirect(writer, request, "/view/" + title, http.StatusFound)
 }
 
 func renderTemplate(err error, w http.ResponseWriter, page *Page, templateName string) {
@@ -51,5 +65,6 @@ func renderTemplate(err error, w http.ResponseWriter, page *Page, templateName s
 func main() {
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 	http.ListenAndServe(":8080", nil)
 }
